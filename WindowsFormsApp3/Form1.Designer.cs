98f63474c32ff9bdc238aa691692cb54bbfb6bec
ATM <cs22918980x@gmail.com>
2018-05-06 12:30:54 +0800

    size

--- WindowsFormsApp3/Form1.Designer.cs
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(686, 392);
+            this.button1.Location = new System.Drawing.Point(493, 466);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -45,7 +45,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1000, 524);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
